38628607ba9b15351bf33d3dcf3e7bdf4ac37626
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2017-01-21 22:19:44 +0300

    Stub version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'std:accounts-semantic',
-  version: '1.1.8',
+  version: '1.1.8_1',
   summary: 'Semantic UI – Accounts UI for React in Meteor 1.3',
   git: 'https://github.com/studiointeract/accounts-semantic',
   documentation: 'README.md'
